c485f9a27ea1895722de122afd4b89c8227b1f59
Joel Moss <joel@developwithstyle.com>
2015-05-03 19:59:32 +0100

    Assets setup

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,22 @@
 // It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
 // compiled file.
 //
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
-//= require turbolinks
+//= require jquery.readyselector
+//= require semantic-ui
+//= require messenger
+//= require messenger-theme-future
+//= require react
+//= require moment
+//= require react_ujs
+//= require lodash
+//= require components
 //= require_tree .
+
+Messenger.options = {
+  extraClasses: 'messenger-fixed messenger-on-bottom'
+}
