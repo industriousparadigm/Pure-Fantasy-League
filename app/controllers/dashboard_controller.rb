afb9fe752e7307525a7e8a2a78f6e64ad1fb0292
Joel Moss <joel@developwithstyle.com>
2015-06-19 16:44:28 +0100

    Couple of fixes to signout state

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < LeagueController
   skip_before_action :authenticate_user!, if: -> { User.count < 1 }
 
   def index
-    unless current_team
+    if current_league && !current_team
       manager = current_league.managers.find_by(user: current_user)
       @team = current_league.teams.build(manager: manager)
     end
